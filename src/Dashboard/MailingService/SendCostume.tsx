912c1137a8c95b4760ebde351ecf55c5fc20096e
medreda2004 <bellati.mohamed.reda16@gmail.com>
2023-05-24 11:17:32 +0100

    commit

--- src/Dashboard/MailingService/SendCostume.tsx
+++ src/Dashboard/MailingService/SendCostume.tsx
@@ -1,17 +1,23 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShare } from "@fortawesome/free-solid-svg-icons";
 import { faMessage } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
 import "../dropdown.css";
 import "../home.css";
 import "../Charts";
 import "./sendcostume.css";
+import JoditEditor from "jodit-react";
+import Button from "@mui/material/Button";
+
 
 // import './script'
 export function SendCostume() {
+  const editor = useRef(null);
   const [show, setShow] = useState(true);
   const [show2, setShow2] = useState(false);
+  const [subject, setSubject] = useState('');
+  const [body, setBody] = useState('');
+  const storedData = localStorage.getItem("userDetails");
 
   const handleShow = () => {
     setShow(true);
@@ -23,6 +29,59 @@ export function SendCostume() {
     setShow2(true);
   };
 
+  const sendEmail = ( emailtype : number ) => {
+
+    if (storedData) {
+        const parsedData = JSON.parse(storedData);
+        const id = parsedData.id;
+        if (emailtype == 0){
+          const data ={
+            "userID" : id,
+            "emailtype" : emailtype,
+            "subject" : "",
+            "body" : "",
+          }
+            const url = `http://localhost:53264/api/email/send`;
+          fetch(url, {
+          method: "POST",
+          headers: {
+            Accept: "application/json, text/plain",
+            "Content-Type": "application/json;charset=UTF-8"
+          },
+          body: JSON.stringify(data),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);      
+          })
+            
+      }else{
+            const data ={
+              "userID" : id,
+              "emailtype" : emailtype,
+              "subject" : subject,
+              "body" : body,
+          }
+          const url = `http://localhost:53264/api/email/send`;
+          fetch(url, {
+          method: "POST",
+          headers: {
+            Accept: "application/json, text/plain",
+            "Content-Type": "application/json;charset=UTF-8"
+          },
+          body: JSON.stringify(data),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);      
+          })
+       }
+       
+        
+}}
+
+
+
   return (
     <div className="table-data">
       <div className="Emails-mang">
@@ -31,6 +90,7 @@ export function SendCostume() {
         </div>
         <form className="costumefrom" action="">
           <div className="message-type">
+      
             <input type="radio" id="type1" name="type" onClick={handleShow} />
             <label htmlFor="type1">Costume Message</label>
 
@@ -39,40 +99,36 @@ export function SendCostume() {
           </div>
           {show && (
             <div>
+             
               <div id="input-feild">
-                <i>
-                  <FontAwesomeIcon icon={faUser} className="icon" />
-                </i>
-                <input type="text" placeholder="To" name="to" />
+                
               </div>
               <div id="input-feild">
                 <i>
-                  <FontAwesomeIcon icon={faShare} className="icon" />
+                  <FontAwesomeIcon icon={faShare} className="icon"/>
                 </i>
-                <input type="text" placeholder="Subject" name="subject" />
+                <input type="text" onChange={(e) => setSubject(e.target.value)} placeholder="Subject" name="subject" />
               </div>
               <div id="input-feild">
                 <i>
                   <FontAwesomeIcon icon={faMessage} className="icon" />
                 </i>
-                <textarea
-                  placeholder="Message"
-                  name="message"
-                  rows={10}
-                ></textarea>
+                <JoditEditor
+                  ref={editor}
+                  value = ''
+                  onChange={(content: any) =>setBody(content)}
+                
+                  />
               </div>
-              <input type="submit" value="Send" className="send-btn" />
+              <Button  onClick={()=>sendEmail(1)}  >Send</Button>
             </div>
           )}
           {show2 && (
             <div>
               <div id="input-feild">
-                <i>
-                  <FontAwesomeIcon icon={faUser} className="icon" />
-                </i>
-                <input type="text" placeholder="To" name="to" />
+                
               </div>
-              <input type="submit" value="Send" className="send-btn" />
+              <Button type="submit" onClick={()=>sendEmail(0)}  >Send</Button>
             </div>
           )}
         </form>
@@ -80,4 +136,5 @@ export function SendCostume() {
     </div>
   );
 }
+
 export default SendCostume;
